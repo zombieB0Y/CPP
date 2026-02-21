c852a8f7f303e82947b52b3198f672451a4f1199
zombieBOY <zoubairgaming5@gmail.com>
2026-02-21 23:44:28 +0000

    cpp06 ex00

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
-#include "class.hpp"
+#include <cerrno>
+#include <cstdlib>
+#include <iomanip>
 
 
-int	main() {
-	base *base1 = new base();
-	child	*child1 = new child();
-	// child1->print();
-	base1->print();
-	base1 = dynamic_cast<base*>(child1);
-	base1->print();
+void	print_character(int c) {
+	std::cout << "char: '" << static_cast<char>(c) << "'\n";
+	std::cout << "int: " << c << "\n";
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "float: " << static_cast<float>(c) << "f\n";
+	std::cout << "double: " << static_cast<double>(c) << "\n";
+}
+
+void	print_non_printable(int c) {
+	std::cout << "char: " << "Non displayable" << "\n";
+	std::cout << "int: " << c << "\n";
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "float: " << static_cast<float>(c) << "f\n";
+	std::cout << "double: " << static_cast<double>(c) << "\n";
+}
+
+void	print_error(std::string msg) {
+	std::cerr << msg << std::endl;
+}
+
+int	main(int ac, char **av) {
+	if (ac == 2) {
+		char *end;
+		double	arg = strtod(av[1], &end);
+		if (av[1][0] == 0)
+			print_error("empty !");
+		else if (av[1] == end) {
+			std::string	arg(av[1]);
+			if (arg.length() > 1)
+				print_error("not valid !");
+			else if (isprint(av[1][0]))
+				print_character(av[1][0]);
+		}
+		else if (errno == ERANGE)
+			print_error("too big \"that what she said ;)\" !");
+		else {
+			if (arg < 127 && isprint(arg))
+				print_character(arg);
+			else
+				print_non_printable(arg);
+		}
+	}
+	else
+		return 1;
 }
