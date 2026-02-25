7dd43e9d4edb50c228b93a4271e3bc51ea228272
zombieBOY <zoubairgaming5@gmail.com>
2026-02-25 00:26:24 +0000

    cpp06 ex00 done !

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -15,10 +15,3 @@ private:
 public:
 	static void	convert(const std::string &s);
 };
-
-
-bool	check_for_numbers(const std::string &s);
-
-void	print_character(int c);
-void	print_non_printable(int c);
-void	print_error(std::string msg);
