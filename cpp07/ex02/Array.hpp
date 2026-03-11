0270f68d4f2a3e52c85f516308937f5cee9b7545
Zoubair Entifi <zoentifi@e1r12p2.1337.ma>
2026-03-11 00:05:13 +0000

    cpp07 done!

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <exception>
 
 template <typename T>
 class Array
@@ -10,20 +11,39 @@ private:
 	T		*arr;
 public:
 	Array() {
-		this->arr = new T();
-		n = 1;
+		this->arr = new T[0];
+		n = 0;
 	}
 	Array(const Array &copy) {
-
+		this->arr = 0;
+		*this = copy;
 	}
-	Array   &operator=(const Array &copy);
-	~Array();
+	Array   &operator=(const Array &copy) {
+		if (this != &copy) {
+			T	*_new = new T[copy.size()]();
+			for (size_t i = 0; i < copy.size(); i++) {
+				_new[i] = copy.arr[i];
+			}
+			delete[] this->arr;
+			this->arr = _new;
+			this->n = copy.size();
+		}
+		return *this;
+	}
+	T	&operator[](const size_t idx) {
+		if (idx >= n)
+			throw std::exception();
+		return this->arr[idx];
+	}
+	~Array() {
+		delete[] this->arr;
+	}
 
 	Array(const size_t n) {
 		this->arr = new T[n]();
 		this->n = n;
 	}
-	size_t	size() {
+	size_t	size() const {
 		return n;
 	}
 };
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,8 +1,38 @@
-#include <iostream>
+#include "Array.hpp"
 
 int main() {
-    std::string *a = new std::string();
-    // for (int i = 0; i < 5; i++) {
-        std::cout << *a << std::endl;
-    // }
+    Array<int> emptyArr;
+    std::cout << emptyArr.size() << std::endl;
+
+    Array<int> arr(5);
+    for (size_t i = 0; i < arr.size(); i++) {
+        arr[i] = i * 2;
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    Array<int> copyArr(arr);
+    arr[0] = 42;
+    std::cout << arr[0] << " " << copyArr[0] << std::endl;
+
+    Array<int> assignArr;
+    assignArr = copyArr;
+    copyArr[1] = 1337;
+    std::cout << copyArr[1] << " " << assignArr[1] << std::endl;
+
+    try {
+        arr[5] = 100;
+        std::cout << "This should not print." << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << "Exception caught: Index out of bounds!" << std::endl;
+    }
+
+    try {
+        emptyArr[0] = 1;
+        std::cout << "This should not print." << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << "Exception caught: Empty array out of bounds!" << std::endl;
+    }
+
+    return 0;
 }
