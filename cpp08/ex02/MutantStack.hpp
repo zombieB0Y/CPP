eab450efdb0846c3c26230aabcf2d456659f4942
Zoubair Entifi <zoentifi@e1r8p5.1337.ma>
2026-06-19 18:02:37 +0100

    ex02 done !

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <algorithm>
+#include <stack>
+#include <list>
+#include <iterator>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+	MutantStack &operator=(const MutantStack &other) {
+		if (this != &other)
+			std::stack<T>::operator=(other);
+		return *this;
+	}
+	~MutantStack() {}
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
+
+	reverse_iterator rbegin() { return this->c.rbegin(); }
+	reverse_iterator rend() { return this->c.rend(); }
+
+	const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+	const_reverse_iterator rend() const { return this->c.rend(); }
+};
