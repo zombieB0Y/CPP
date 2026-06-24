9c0b87bc66f1f71ad097b6db6f9bfa109e1b227b
Zoubair Entifi <zoentifi@c4r5p8.1337.ma>
2026-06-24 18:01:23 +0100

    still parsing input file

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -11,6 +11,8 @@ BitcoinExchange::BitcoinExchange() {
 	std::getline(data_f, line);
 	int	i = 0;
 	while (std::getline(data_f, line)) {
+		if (line == "")
+			break;
 		i = 0;
 		std::istringstream	line_s(line);
 		std::string			token;
@@ -48,4 +50,111 @@ void	BitcoinExchange::print_obj() {
 	for (size_t i = 0; i < this->dates.size(); i++) {
 		std::cout << this->dates[i] << " | " << std::fixed << std::setprecision(2) << this->values[i] << std::endl;
 	}
+}
+
+int	BitcoinExchange::calcule_PNL(std::string input_file) {
+	std::deque<std::string>	dq = this->load_input(input_file);
+	if (dq.at(0) != "data" || dq.at(1) != "value") {
+		std::cerr << "Erorr: bad header input.\n";
+		dq.pop_front();
+		dq.pop_front();
+	}
+	bool	odd = false;
+	if (dq.size() % 2 == 1) {
+		std::string	last_elem = dq.back();
+		dq.pop_back();
+		odd = true;
+	}
+	while (dq.size()) {
+		if (_handel_date(dq.at(0)) && _handel_value(dq.at(1))) {
+
+		}
+	}
+}
+
+bool	_handel_date(std::string date) {
+	std::istringstream	line_s(date);
+	std::string			token;
+	int	i = 0;
+	while (std::getline(line_s, token, '-')) {
+		if (token.empty()) return false;
+		if (i == 0)
+			if (!check_years(token)) return false;
+		else if (i == 1)
+			if (!check_month(token)) return false;
+		else if (i == 2)
+			if (!check_days(token)) return false;
+		else if (i > 2) return false;
+		++i;
+	}
+}
+
+bool	_handel_value(std::string value) {
+	
+}
+
+bool	check_days(std::string days) {
+
+}
+
+bool	check_month(std::string month) {
+	size_t 		i = 0;
+	long long	count = 0;
+	while (i < month.length()) {
+		if (!isdigit(month.at(i))) return false;
+		count = count * 10 + (month.at(i) - '0');
+		if (count > std::numeric_limits<int>::max()) return false;
+		++i;
+	}
+	
+}
+
+bool	check_years(std::string year) {
+	while (!year.empty()) {
+		if (!isdigit(year.at(0))) return false;
+		year.erase(0, 1);
+	}
+	return true;
+}
+
+std::deque<std::string>	BitcoinExchange::load_input(std::string input_file) {
+	std::ifstream ip_file(input_file);
+	if (!ip_file.is_open()) {
+		std::cerr << "Error: could not open file.\n";
+		throw std::exception();
+	}
+	std::string	line;
+	// std::getline(ip_file, line);
+	std::deque<std::string> dq;
+	int i = 0;
+	while (std::getline(ip_file, line)) {
+		if (line.empty()) continue;
+		std::istringstream	line_s(line);
+		std::string			token;
+		i = 0;
+		while (std::getline(line_s, token, '|')) {
+			if (token.empty()) {
+				std::cerr << "Error: bad input => " << line << std::endl;
+				// throw std::exception();
+				break;
+			}
+			if (i > 1) {
+				std::cerr << "Error: bad input => " << line << std::endl;
+				// throw std::exception();
+				dq.pop_back();
+				dq.pop_back();
+				break;
+			}
+			token = _trim(token);
+			dq.push_back(token);
+			++i;
+		}
+	}
+	return dq;
+}
+
+std::string	_trim(std::string& input) {
+	std::string::size_type start = input.find_first_not_of(" \t\n\r\v\f");
+	std::string::size_type end = input.find_last_not_of(" \t\n\r\v\f");
+	return input.substr(start, end - start + 1);
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <cerrno>
+#include <cstdlib>
+#include <limits>
 #include <string>
 #include <sstream>
 #include <algorithm>
@@ -21,5 +24,11 @@ public:
 	BitcoinExchange& operator=(const BitcoinExchange& other);
 	~BitcoinExchange();
 
-	void	print_obj();
+	void		print_obj();
+	std::deque<std::string>	load_input(std::string input_file);
+	int			calcule_PNL(std::string input_file);
 };
+
+std::string	_trim(std::string& input);
+bool		_handel_date(std::string date);
+bool		_handel_value(std::string value);
